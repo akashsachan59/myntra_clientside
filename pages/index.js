94c0cb3e4d697f754adc136eae68e67722785f23
Akash Sachan <akashsachan@Akashs-MacBook-Air.local>
2022-05-04 21:57:36 +0530

    refined search

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Search from './search'
 
 export default function Home({ data }) {
--- pages/search.js
+++ pages/search.js
@@ -1,12 +1,75 @@
-export default function Search(){
-    return (
-        <div className="search-container">
-            <div className="search-box">
-                <input type="text" placeholder="Search for products" />
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export default function Search({ data }) {
+    
+    const [search, setSearch] = useState('');
+    
+    const router = useRouter();
+    
+    // handle search input
+    const handleInput = (e) => {
+        setSearch(e.target.value)
+    }
+
+    // handle search button
+    const handleSubmit = () => {
+        router.query.search = search;
+        router.push(`/search?search=${search}`);
+        }
+    
+        
+    if(!data){
+        return (
+            <div>
+            <div className="search-container">
+                <div className="search-box">
+                    <input type="text" placeholder="Search for products" onChange={handleInput} />
+                </div>
+                <div className="search-button">
+                    <button onClick={handleSubmit}>Search</button>
+                </div>
             </div>
-            <div className="search-button">
-                <button>Search</button>
             </div>
+            )
+    }else{
+        return (
+            <div>
+            <div className="search-container">
+                <div className="search-box">
+                    <input type="text" placeholder="Search for products" onChange={handleInput} />
+                </div>
+                <div className="search-button">
+                    <button onClick={handleSubmit}>Search</button>
+                </div>
+            </div>
+            <div className="search-results">
+            {data.map((item) => (
+                <ul key={item.id}>
+                    <li>{item.name}</li>
+                    <li>{item.category}</li>
+                    <li>{item.price}</li>
+                    <li>{item.gender}</li>
+                    <img src={item.image} height="180px" width="135px"/>
+                </ul>
+            ))}
         </div>
-    )
+        </div>    
+            )
+    }   
+}
+
+export async function getServerSideProps(context) {
+    const res = await fetch(`http://localhost:3000/search?search=${context.query.search}`)
+    const data = await res.json()
+
+    if (!data) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: { data }
+    }
 }
