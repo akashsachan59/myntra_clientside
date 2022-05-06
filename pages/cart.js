5b4888c8b4698e3030e60566d127e5ff869d219b
Akash Sachan <akashsachan@Akashs-MacBook-Air.local>
2022-05-06 23:23:34 +0530

    added remove product

--- pages/cart.js
+++ pages/cart.js
@@ -4,19 +4,47 @@ export default function Cart(data) {
     let router = useRouter()
     console.log(router.query.user)
     console.log(data.data)
+    
+    // handle remove from cart
+    const remove = async (e) => {
+        e.preventDefault()
+        let id = e.target.value
+        let user_id = router.query.user
+        const data = {
+            id,
+            user_id
+        }
+        const response = await fetch('http://localhost:3000/deleteCart', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const resJson = await response.json()
+        console.log(resJson)
+        if (resJson.res) {
+            router.push(`/cart?user=${user_id}`)
+        }
+    }
+    
     if(data){
         return(
             <div>
                 <h1>CART</h1>
                 <div className="cart-container">
                     {data.data.map(item => (
-                         <ul key={item.id}>
-                         <li>{item.name}</li>
-                         <li>{item.category}</li>
-                         <li>{item.price}</li>
-                         <li>{item.gender}</li>
-                         <img src={item.image} height="180px" width="135px"/>
-                       </ul>
+                        <div key={item.id}>
+                            <ul>
+                                <li>{item.name}</li>
+                                <li>{item.category}</li>
+                                <li>{item.price}</li>
+                                <li>{item.gender}</li>
+                                <li>{item.id}</li>
+                                <img src={item.image} height="180px" width="135px" />
+                            </ul>
+                            <button onClick={remove} value={item.id}>delete from Cart</button>
+                        </div>
                     ))}
                 </div>
             </div>
